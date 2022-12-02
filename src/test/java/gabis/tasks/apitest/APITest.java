77581820bb00f9a37bd31c92f2e67c758c7d5750
GabisEngDB <gabriel.leite@engdb.com.br>
2022-12-02 09:51:56 -0300

    first commit

---
+++ src/test/java/gabis/tasks/apitest/APITest.java
@@ -0,0 +1,53 @@
+package gabis.tasks.apitest;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class APITest {
+	
+	@BeforeClass
+	public static void setup() {
+		RestAssured.baseURI = "http://localhost:8001/tasks-backend";
+	}
+	
+	@Test
+	public void deveRetornarTarefas() {
+		RestAssured.given()
+		.when()
+			.get("/todo")
+		.then()
+			.statusCode(200)
+		;
+	}
+	
+	@Test
+	public void deveRetornarTarefaComSucesso() {
+		RestAssured.given()
+			.body("{ \"task\": \"Test via API\", \"dueDate\": \"2022-12-03\" }")
+			.contentType(ContentType.JSON)
+		.when()
+			.post("/todo")
+		.then()
+			.statusCode(201)
+		;
+	}
+	
+	@Test
+	public void naoDeveAdicionarTarefaInvalida() {
+		RestAssured.given()
+			.body("{ \"task\": \"Test via API\", \"dueDate\": \"2012-12-03\" }")
+			.contentType(ContentType.JSON)
+		.when()
+			.post("/todo")
+		.then()
+			.log().all()
+			.statusCode(400)
+			.body("message", CoreMatchers.is("Due date must not be in past"))
+		;
+	}
+
+}
